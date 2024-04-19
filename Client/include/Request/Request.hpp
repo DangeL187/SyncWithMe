795687c209d02cc37c70cb86bf86c5e292f5fe89
DangeL <92173667+DangeL187@users.noreply.github.com>
2024-04-19 17:14:47 +0300

    Docs Request.hpp

--- Client/include/Request/Request.hpp
+++ Client/include/Request/Request.hpp
@@ -7,6 +7,35 @@
 #include "dtl.hpp"
 #include "json.hpp"
 
+/*! @class Request
+    @brief Class that describes specific request that can be sent to show file changes
+*/
+/*! @fn Request::Request()
+    @brief Create invalid request
+*/
+/*! @fn Request::Request(const std::string& shared_file_path, const std::string& old_file_path, const std::string& new_file_path)
+    @brief Create request that describes how the file was changed
+    @param shared_file_path File path but related to the <b> shared </b> folder
+    @param old_file_path ?
+    @param new_file_path ?
+*/
+/*! @var Request::_data
+    @brief Contains information about file's recent changes
+    @det Uses data type from dtl.hpp
+*/
+/*! @var Request::_file_path
+    @brief Contains shared file path 
+*/
+/*! @var Request::_is_deleted
+    @brief Was file deleted or not 
+*/
+/*! @var Request::_is_valid
+    @brief Is request valid. It is invalid only when creating Request like this - Request() 
+*/
+/*! @var Request::_new_file_name
+    @brief File's new name after renaming
+*/
+
 class Request {
 public:
     Request();
